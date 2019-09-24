aced60ff6a5840a446c10e93a57cb252ca1794a9
Kevin-OuY <kevin.ouy.hn@outlook.com>
2019-09-24 23:30:46 +0800

    目录模板编辑

--- src/views/contentTemplate/contentTemplate.vue
+++ src/views/contentTemplate/contentTemplate.vue
@@ -2,104 +2,81 @@
   <div class="pos-rltv padding-20">
     <h2>分类目录模板</h2>
     <div>
-      <el-tree :data="setTree" :props="defaultProps"></el-tree>
+      <el-tree 
+        :data="categoryTree" 
+        :props="categoryProps"
+        @node-click="selectCategory">
+      </el-tree>
     </div>
 
     <hr>
+
     <div v-loading="isLoading" class="comp-tree">
+      <el-button class="comp-tr-top" 
+        type="primary" 
+        size="small" 
+        @click="handleAddTop">添加顶级节点</el-button>
       <!-- tree -->
-		  <el-tree ref="SlotTree"
+      <el-tree ref="SlotTree"
         :data="setTree"
         :props="defaultProps"
         :expand-on-click-node="false"
         highlight-current
-        :node-key="NODE_KEY"
-        :default-expanded-keys="expanded">
-				<div class="comp-tr-node" slot-scope="{ node, data }">
-					<!-- 编辑状态 -->
-					<template v-if="node.isEdit">
-						<el-input v-model="data.name" 
-							autofocus
-							size="mini"
-							:ref="'slotTreeInput'+data[NODE_KEY]"
-							@blur.stop="handleInput(node, data)"
-							@keyup.enter.native="handleInput(node, data)"></el-input>
-					</template>
+        :node-key="NODE_KEY">
+        <div class="comp-tr-node" slot-scope="{ node, data }">
+          <!-- 编辑状态 -->
+          <template v-if="node.isEdit">
+            <el-input v-model="data.contentName" 
+              autofocus
+              size="mini"
+              :ref="'slotTreeInput'+data[NODE_KEY]"
+              @blur.stop="handleInput(node, data)"
+              @keyup.enter.native="handleInput(node, data)"></el-input>
+          </template>
 
-					<!-- 非编辑状态 -->
-					<template v-else>
-						<!-- 名称： 新增节点增加class（is-new） -->
-						<span :class="[data[NODE_KEY] < NODE_ID_START ? 'is-new' : '', 'comp-tr-node--name']">
-							{{ node.label }}
-						</span>
-						
-						<!-- 按钮 -->
-						<span class="comp-tr-node--btns">
-							<!-- 新增 -->
-							<el-button icon="el-icon-plus" 
-								size="mini"
-								circle 
-								type="primary"
+          <!-- 非编辑状态 -->
+          <template v-else>
+            <!-- 名称： 新增节点增加class（is-new） -->
+            <span :class="[data[NODE_KEY] < NODE_ID_START ? 'is-new' : '', 'comp-tr-node--name']">
+              {{ node.label }}
+            </span>
+            
+            <!-- 按钮 -->
+            <span class="comp-tr-node--btns">
+              <!-- 新增 -->
+              <el-button icon="el-icon-plus" 
+                size="mini"
+                circle 
+                type="primary" 
                 title="新增"
-								@click="handleAdd(node, data)"></el-button>
+                @click="handleAdd(node, data)"></el-button>
 
-							<!-- 编辑 -->
-							<el-button icon="el-icon-edit" 
-								size="mini"
-								circle 
-								type="info"
+              <!-- 编辑 -->
+              <el-button icon="el-icon-edit" 
+                size="mini"
+                circle 
+                type="info" 
                 title="修改"
-								@click="handleEdit(node, data)"></el-button>
+                @click="handleEdit(node, data)"></el-button>
 
-							<!-- 删除 -->
-							<el-button icon="el-icon-delete" 
-								size="mini"
-								circle 
-								type="danger"
+              <!-- 删除 -->
+              <el-button icon="el-icon-delete" 
+                size="mini"
+                circle 
+                type="danger" 
                 title="删除"
-								@click="handleDelete(node, data)"></el-button>
-						</span>
-					</template>
-				</div>
-			</el-tree>
+                @click="handleDelete(node, data)"></el-button>
+            </span>
+          </template>
+        </div>
+      </el-tree>
 	  </div>
-
-    <!-- <el-button class="diag-btn" type="text" @click="dialogFormVisible = true">打开嵌套表单的 Dialog</el-button> -->
-
-    <el-dialog 
-      :title="diagTitle.text" 
-      :visible.sync="dialogFormVisible">
-      <el-form :model="form" :rules="rules" ref="ruleForm" :label-position="labelPos">
-        <el-form-item label="上级目录" :label-width="formLabelWidth" prop="parentId">
-          <el-cascader
-            :value="form.parentId"
-            :options="options"
-            :props="cascaderProps"
-            @change="handleChange"></el-cascader>
-        </el-form-item>
-        <el-form-item label="分类名称" :label-width="formLabelWidth" prop="name">
-          <el-input v-model="form.name" autocomplete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="分类编码" :label-width="formLabelWidth">
-          <el-input v-model="form.code" autocomplete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="备注" :label-width="formLabelWidth">
-          <el-input v-model="form.descM" autocomplete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="排序" :label-width="formLabelWidth" prop="sort">
-          <el-input v-model.number="form.sort" autocomplete="off"></el-input>
-        </el-form-item>
-      </el-form>
-      <div slot="footer" class="dialog-footer">
-        <el-button @click="dialogFormVisible = false">取 消</el-button>
-        <el-button type="primary" @click="doSaveAction">确 定</el-button>
-      </div>
-    </el-dialog>
   </div>
 </template>
 
 <script>
-import api from '@/api/contentTemplate/index.js'
+import templateApi from '@/api/contentTemplate/index.js'
+import categoryApi from '@/api/categoryManager/index.js'
 import _ from 'lodash'
 
 export default{
@@ -107,8 +84,9 @@ export default{
 	data(){
 		return {
 			isLoading: false,// 是否加载
-			setTree: [],
-			NODE_KEY: 'id',// id对应字段
+      setTree: [], // 目录树
+      categoryTree: [], // 
+			NODE_KEY: 'categoryId',// id对应字段
 			MAX_LEVEL: 3,// 设定最大层级
 			NODE_ID_START: 0,// 新增节点id，逐次递减
 			// startId: null,
@@ -116,11 +94,20 @@ export default{
 				children: 'children',
 				label: 'contentName'
       },
+      categoryProps: {
+        children: 'children',
+				label: 'name'
+      },
       expanded: [], // 默认展开
       dialogFormVisible: false, // diag可见性
       formLabelWidth: '80px',
       labelPos: 'right',
       diagTitle: {edit: '编辑分类', add: '添加分类', text: ''},
+      initParam: {// 新增参数
+				contentName: '新增节点',
+				pid: 0,
+				children: []
+			},
       form: {
         id: undefined,
         name: '',
@@ -145,168 +132,105 @@ export default{
           { type: 'number', message: '排序值必须为数字', trigger: 'blur' }
         ]
       },
-      cascaderProps: {
-        label: 'contentName',
-        value: 'id',
-        checkStrictly: true
-      },
       options: []
 		}
 	},
 	created(){
 		// 初始值
-    // this.startId = this.NODE_ID_START 
-    this.getTreeData()
+    this.startId = this.NODE_ID_START 
+    this.getCategoryTreeData()
+    this.getTemplateTreeData()
 	},
 	methods: {
-    // 删除category
-		handleDelete(_node, _data){
-      let vm = this;
+    handleDelete(_node, _data){// 删除节点
 			console.log(_node, _data)
-			// 判断是否存在子category
+			// 判断是否存在子节点
 			if(_data.children && _data.children.length !== 0){
-				this.$message.error("此分类有子级，不可删除！", 5000)
+				this.$message.error("此节点有子级，不可删除！")
 				return false;
 			}else{
-				// // 删除操作
-				// let DeletOprate = () => {
-				// 	this.$nextTick(() => {
-				// 		if(this.$refs.SlotTree){
-				// 			this.$refs.SlotTree.remove(_data)
-				// 			this.$message.success("删除成功！")
-				// 		}
-				// 	})
-				// }
-
+				// 删除操作
+				let DeletOprate = () => {
+					this.$nextTick(() => {
+						if(this.$refs.SlotTree){
+							this.$refs.SlotTree.remove(_data)
+							this.$message.success("删除成功！")
+						}
+					})
+				}
 				// 二次确认
 				let ConfirmFun = () => {
-					this.$confirm("是否删除此分类？","提示",{
+					this.$confirm("是否删除此节点？","提示",{
 						confirmButtonText: "确认",
 						cancelButtonText: "取消",
 						type: "warning"
 					}).then(() => {
-            // DeletOprate()
-            vm.expanded = [].concat(_data.parentId)
-            vm.doDeleteAction(_data.id)
+						DeletOprate()
 					}).catch(() => {})
 				}
-
 				// 判断是否新增： 新增节点直接删除，已存在的节点要二次确认
-        // _data[this.NODE_KEY] < this.NODE_ID_START ? DeletOprate() : ConfirmFun()
-        ConfirmFun()
+				_data[this.NODE_KEY] < this.NODE_ID_START ? DeletOprate() : ConfirmFun()
 			}
-    },
-    // 修改category
-		handleInput(_node, _data){
+		},
+		handleInput(_node, _data){// 修改节点
 			console.log(_node, _data)
 			// 退出编辑状态
 			if(_node.isEdit){
 				this.$set(_node, 'isEdit', false)
 			}
-    },
-    // 编辑category
-		handleEdit(_node, _data){
-      console.log(_node, _data)
-
-      // 判断层级
-			if(_node.level > this.MAX_LEVEL){
-				this.$message.warning("第3级以上数据暂时不作修改，以免影响数据结构")
-				return false;
-      }
-
-      this.clickSlot('edit', _data);
-      return ;
-
-			// // 设置编辑状态
-			// if(!_node.isEdit){
-			// 	this.$set(_node, 'isEdit', true)
-			// }
-
-			// // 输入框聚焦
-			// this.$nextTick(() => {
-			// 	if(this.$refs['slotTreeInput'+_data[this.NODE_KEY]]){
-			// 		this.$refs['slotTreeInput'+_data[this.NODE_KEY]].$refs.input.focus()
-			// 	}
-			// })
-    },
-    // 新增category
-		handleAdd(_node, _data){
-			console.log('add: ', _node, _data)
+		},
+		handleEdit(_node, _data){// 编辑节点
+			console.log(_node, _data)
+			// 设置编辑状态
+			if(!_node.isEdit){
+				this.$set(_node, 'isEdit', true)
+			}
+			// 输入框聚焦
+			this.$nextTick(() => {
+				if(this.$refs['slotTreeInput'+_data[this.NODE_KEY]]){
+					this.$refs['slotTreeInput'+_data[this.NODE_KEY]].$refs.input.focus()
+				}
+			})
+		},
+		handleAdd(_node, _data){// 新增节点
+			console.log(_node, _data)
 			// 判断层级
 			if(_node.level >= this.MAX_LEVEL){
-				this.$message.warning("当前暂只支持"+ this.MAX_LEVEL + "级，无法继续新增！")
+				this.$message.warning("当前已达到"+ this.MAX_LEVEL + "级，无法新增！")
 				return false;
-      }
-
-
-      this.clickSlot('add', _data)
-      return ;
-
-			// // 参数修改
-			// let obj = JSON.parse(JSON.stringify(this.initParam));// copy参数
-			// obj.parentId = _data[this.NODE_KEY];// 父id
-			// obj[this.NODE_KEY] = --this.startId;// 节点id：逐次递减id
-			// // 判断字段是否存在
-			// if(!_data.children){
-			// 	this.$set(_data, 'children', [])
-			// }
-			// // 新增数据
-			// _data.children.push(obj)
-
-			// // 展开节点
-			// if(!_node.expanded){
-			// 	_node.expanded = true
-			// }
-    },
-    // 添加一级category
-		handleAddTop(){
-			// let obj = JSON.parse(JSON.stringify(this.initParam));// copy参数
-			// obj[this.NODE_KEY] = --this.startId;// categoryid：逐次递减id
-      // this.setTree.push(obj)
-      this.form = {}
-      this.clickSlot('add', {id: '', grade: 0})
-    },
-    // 拉取树形数据，带后续处理
-    getTreeData(){
+			}
+			// 参数修改
+			let obj = JSON.parse(JSON.stringify(this.initParam));// copy参数
+			obj.pid = _data[this.NODE_KEY];// 父id
+			obj[this.NODE_KEY] = --this.startId;// 节点id：逐次递减id
+			// 判断字段是否存在
+			if(!_data.children){
+				this.$set(_data, 'children', [])
+			}
+			// 新增数据
+			_data.children.push(obj)
+			// 展开节点
+			if(!_node.expanded){
+				_node.expanded = true
+			}
+		},
+		handleAddTop(){// 添加顶部节点
+			let obj = JSON.parse(JSON.stringify(this.initParam));// copy参数
+			obj[this.NODE_KEY] = --this.startId;// 节点id：逐次递减id
+			this.setTree.push(obj)
+		},
+    // 拉取categoryTree数据，带后续处理
+    getCategoryTreeData(){
       let vm = this
       vm.isLoading = true
-      api.getTreeData()
+      categoryApi.getTreeData()
       .then(res => {
         console.log('success:', res);
         // return;
         if(res.status == 'success'){
           let data = res.data && (_.cloneDeep(res.data.children) || [])
-
-          if(data.length){
-            data.forEach(x => {
-              // 第一级
-              x.disabled = false;
-              if(x.children && x.children.length){
-                x.children.forEach(y => {
-                  // 第二级
-                  // if(y.children && y.children.length){
-                  //   y.children.forEach(z => {
-                  //     // 第三级
-                  //     z.children  && (delete z.children)
-                  //   })
-                  // }
-                  // else{
-                  //   y.children && (delete y.children)
-                  // }
-                  y.disabled = false
-                  y.children && (delete y.children)
-                });
-              }else{
-                x.children && (delete x.children)
-              }
-            });
-
-            this.setTree = res.data.children
-          }
-
-          this.options = [{children: data || [], id: 'ROOT', contentName: '根目录', disabled: false}]
-        
-          vm.isLoading = false
+          vm.categoryTree = data;
+          vm.isLoading = false;
         }else{
           this.$message.error("获取分类信息失败，请稍后重试！")
         }
@@ -316,41 +240,39 @@ export default{
         this.$message.error("请求出错，错误原因： " + res.msg ? res.msg : JSON.stringify(res));
       })
     },
-    // 级联选择父级category
-    handleChange(e){
-      console.log(e)
+    getTemplateTreeData(){
+      let vm = this
+      vm.isLoading = true
+      templateApi.getTreeData()
+      .then(res => {
+        console.log('success:', res);
+        // return;
+        if(res.status == 'success'){
+          let data = res.data && (_.cloneDeep(res.data.children) || [])
+          vm.setTree = data;
+          vm.isLoading = false;
+        }else{
+          this.$message.error("获取目录模板信息失败，请稍后重试！")
+        }
+      })
+      .catch(e => {
+        // console.log('error: ', res)
+        this.$message.error("请求出错，错误原因： " + e.msg ? e.msg : JSON.stringify(e));
+      })
     },
-    // 处理category事件
-    clickSlot(type, _data){
-
-      this.diagTitle.text = this.diagTitle[type]
-
-      switch(type){
-        case 'edit': {
-          _.merge(this.form, _data)
-          this.form =  _.pick(this.form, ['id', 'parentId', 'code', 'name', 'grade', 'sort', 'descM'])
-
-          this.disableNode(_data.id)
-
-          this.dialogFormVisible = true;
-
-          break;
-        }
-        case 'add': {
-          this.form = {}
-          _.merge(this.form, {
-            parentId: _data.id,
-            grade: _data.grade + 1
-          })
-          this.disableNode()
-
-          this.dialogFormVisible = true;
-
-          break;
-        }
-        default: break;
-      }
-      
+    // 处理选择目录事件
+    selectCategory(data, node, component){
+      // console.log(data, node, component);
+      templateApi.checkTemplateTreeData({
+        id: data.id
+      })
+      .then(res => {
+        // console.log(res);
+        res.data && (this.setTree = res.data)
+      })
+      .catch(e => {
+        this.$message.error("请求出错，错误原因： " + e.msg ? e.msg : JSON.stringify(e));
+      })
     },
     // 发送保存请求操作
     doSaveAction(){
@@ -447,7 +369,7 @@ export default{
 		width: 100%;
 		max-width: 300px;
 		max-height: 80vh;
-    overflow: hidden show;
+    overflow: visible;
   
 		// 顶部按钮
 		.comp-tr-top{
