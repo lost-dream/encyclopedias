38cd3bc4552daefaed3907c225abb1a69422dc3f
Hongyu Lin <32406825+nassatar@users.noreply.github.com>
2019-09-28 14:45:55 +0800

    repaire ckeditor bug

--- src/views/entry/editEntry.vue
+++ src/views/entry/editEntry.vue
@@ -516,12 +516,19 @@
                 // 两级目录间有内容，切割长度为奇数；无内容为偶数
                 r.map((item,index) => {
                     let obj = {}
-                    if(index == 0&&(item.includes('</')&&item.includes('>'))){
-                        obj.title = ''
-                        // obj.plate1 = index/2 + 1
-                        obj.children = []
-                        arr_main.push(obj)
-                        arr_sub.push(r[index].split(reg2))
+                    if(index == 0){
+                        if(item.includes('</')&&item.includes('>')){
+                            obj.title = ''
+                            // obj.plate1 = index/2 + 1
+                            obj.children = []
+                            arr_main.push(obj)
+                            arr_sub.push(r[index].split(reg2))
+                        } else {
+                            obj.title = item
+                            // obj.plate1 = index/2 + 1
+                            obj.children = []
+                            arr_main.push(obj)
+                        }
                     }
                     if(index > 0){
                         if(!(item.includes('</')&&item.includes('>'))){
@@ -536,6 +543,7 @@
                     }
                 })
                 // 构建二级目录结构
+                console.log(arr_sub)
                 arr_sub.map((item,index)=>{
                     if(item.length%2==1){
                         // 如果一级目录和二级目录之间有内容，则将一级目录content设置为中间部分的内容
