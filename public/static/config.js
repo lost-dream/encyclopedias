a12e97d84bad5d17d478beac204e824f51408749
lost-dream <13623673118@163.com>
2020-01-06 09:08:36 +0800

    rewrite -- login 地址添加到配置文件

--- public/static/config.js
+++ public/static/config.js
@@ -24,7 +24,7 @@ var baseUrlConfig = {
   // UPLOAD_URL: 'http://106.12.208.84:8888/wiki-backend/upload/uploadImg'
 
   //192
-  serviceBaseUrl: 'http://192.168.1.186:8080',
+  serviceBaseUrl: 'http://192.168.1.82:8080',
   IMG_PREFIX: 'http://192.168.1.106:8888',
   UPLOAD_URL: 'http://192.168.1.82:8080/wiki-backend/upload/uploadImg',
   LOGIN_URL: 'http://192.168.1.186:8081/text/aa'
@@ -33,6 +33,7 @@ var baseUrlConfig = {
   // serviceBaseUrl: 'http://192.168.0.35:8080',
   // IMG_PREFIX: 'http://192.168.0.35:8888',
   // UPLOAD_URL: 'http://192.168.0.35:22122/wiki-backend/upload/uploadImg'
+  // LOGIN_URL: 'http://192.168.1.186:8081/text/aa'
 
   //10.24.1.21:8080
   //	serviceBaseUrl:'http://10.24.1.21:8080',
